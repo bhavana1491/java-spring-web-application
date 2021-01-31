5e077d4a72d16be265634d80df2a940369516b55
Bhavana Prakash <bhavanap1491@gmail.com>
2021-01-31 02:06:39 -0800

    first commit

---
+++ src/main/java/com/example/simplewebapplication/SimpleWebApplication.java
@@ -0,0 +1,31 @@
+package com.example.simplewebapplication;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SimpleWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx)
+	{
+		return args -> {
+
+			System.out.println("Lets see Spring Bean Names");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			for(String beanName : beanNames)
+			{
+				System.out.println(beanName);
+			}
+		};
+	}
+
+}
